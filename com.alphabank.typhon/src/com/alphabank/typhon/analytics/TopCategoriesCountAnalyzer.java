25c5303f497a4bb133ce8d7c70e3c7d9404cd2a3
Thanos Zolotas <zolotas4@gmail.com>
2019-05-22 14:05:22 +0100

    Top categories count tasks implementation.

--- com.alphabank.typhon/src/com/alphabank/typhon/analytics/TopCategoriesCountAnalyzer.java
+++ com.alphabank.typhon/src/com/alphabank/typhon/analytics/TopCategoriesCountAnalyzer.java
@@ -1,12 +1,17 @@
 package com.alphabank.typhon.analytics;
 
+import java.util.ArrayList;
+
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.functions.RichMapFunction;
 import org.apache.flink.api.java.tuple.Tuple;
 import org.apache.flink.api.java.tuple.Tuple1;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction.Context;
@@ -20,17 +25,21 @@ import com.alphabank.typhon.extractor.insert.FinancialEventInsertExtractor;
 
 import ac.uk.york.typhon.analytics.commons.datatypes.events.Event;
 import ac.uk.york.typhon.analytics.process.StreamAnalyzer;
+import akka.stream.scaladsl.BroadcastHub.Open;
 
 public class TopCategoriesCountAnalyzer extends StreamAnalyzer {
 
 	@Override
 	public DataStream<Event> analyse(DataStream<Event> postEvents) throws Exception {
-		DataStreamSink<Tuple3<String, String, Long>> results = postEvents
+//		DataStreamSink<ArrayList<Tuple3<String, String, Long>>> results = 
+		postEvents
 		.filter(new FilterFunction<Event>() {
 			
 			@Override
-			public boolean filter(Event arg0) throws Exception {
-				if (arg0.getQuery().toLowerCase().contains("insert into table fnc_ev")) {
+			public boolean filter(Event preEvent) throws Exception {
+				String query = preEvent.getQuery().toLowerCase();
+				if (preEvent.getQuery().toLowerCase()
+						.contains("insert into fnc_ev")) {
 					return true;
 				}
 				return false;
@@ -50,6 +59,34 @@ public class TopCategoriesCountAnalyzer extends StreamAnalyzer {
 		.keyBy("mcgDescription")
 		.timeWindow(Time.days(30))
 		.process(new MyProcessWindowFunction())
+//		.addSink(new SinkFunction<Tuple3<String,String,Long>>() {
+//			
+//			@Override
+//			public void invoke(Tuple3<String, String, Long> value) throws Exception {
+//				// TODO Auto-generated method stub
+//				SinkFunction.super.invoke(value);
+//				ArrayList<Tuple3<String,String,Long>> allTuples = new ArrayList<Tuple3<String,String,Long>>();
+//				allTuples.add(value);
+//				System.out.println("Value: " +  value);
+//			}
+//		});
+		
+		.map(new RichMapFunction<Tuple3<String,String,Long>, ArrayList<Tuple3<String,String,Long>>>() {
+
+			ArrayList<Tuple3<String,String,Long>> allTuples;
+			
+			@Override
+			public void open(Configuration parameters) throws Exception {
+				super.open(parameters);
+				allTuples = new ArrayList<Tuple3<String,String,Long>>();
+			}
+			
+			@Override
+			public ArrayList<Tuple3<String, String, Long>> map(Tuple3<String, String, Long> arg0) throws Exception {
+				allTuples.add(arg0);
+				return allTuples;
+			}
+		})
 		.print();
 		
 		return postEvents;
@@ -69,12 +106,30 @@ public class TopCategoriesCountAnalyzer extends StreamAnalyzer {
 			result.f0 = (String)((Tuple1)key).f0;;
 			result.f1 = month;
 			result.f2 = count;
-			System.out.println(key + " " + month + " " + count);
+//			System.out.println(key + " " + month + " " + count);
 			out.collect(result);
 			
 		}
+	}
+	
+	public class CategoriesCountTuple implements Comparable<CategoriesCountTuple> {
+	    private final String name;
+	    private final String month;
+	    private final Long count;
 
-		
+	    public CategoriesCountTuple(String name, String month, long count) {
+	        this.name = name;
+	        this.month = month;
+	        this.count = count;
+	    }
+
+	    public String name() { return name;  }
+	    public String month() { return month;  }
+	    public long count()   { return count; }
+
+	    public int compareTo(CategoriesCountTuple o) {
+	        return this.count.compareTo(o.count);
+	    }
 	}
 
 }
