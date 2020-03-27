a3c9827edb9c6733ec512f1e7ee8782a93f9e1fe
Thanos Zolotas <zolotas4@gmail.com>
2020-03-27 15:38:11 +0000

    Using Java properties to store constants for configuring the simulator.

--- ac.york.typhon.analytics.examples.eshop/src/infra/RunSimulator.java
+++ ac.york.typhon.analytics.examples.eshop/src/infra/RunSimulator.java
@@ -1,11 +1,14 @@
 package infra;
 
-import actions.Action;
-import queryGenerators.InsertCommentGenerator;
+import java.io.InputStream;
+import java.util.Properties;
+
 import queryGenerators.InsertProductGenerator;
 import queryGenerators.InsertUserGenerator;
 
 public class RunSimulator {
+	
+	
 	
 	public static final Boolean GENERATE_USERS = true;
 	public static final Boolean GENERATE_PRODUCTS = true;
@@ -13,17 +16,20 @@ public class RunSimulator {
 	public static final int NUMBER_OF_PRODUCTS = 30;
 
 	public static void main(String[] args) throws Exception {
-
-		if(GENERATE_USERS) {
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties");
+		Properties appProps = new Properties();
+		appProps.load(inputStream);
+		
+		if(Boolean.parseBoolean(appProps.getProperty("generate_users"))) {
 			InsertUserGenerator iug = new InsertUserGenerator();
-			for (int i=0; i < NUMBER_OF_USERS; i++) {
+			for (int i=0; i < Integer.parseInt(appProps.getProperty("num_of_users")); i++) {
 				System.out.println(iug.generateQuery());
 			}
 		}
 		
-		if(GENERATE_PRODUCTS) {
+		if(Boolean.parseBoolean(appProps.getProperty("generate_products"))) {
 			InsertProductGenerator ipg = new InsertProductGenerator();
-			for (int i=0; i < NUMBER_OF_PRODUCTS; i++) {
+			for (int i=0; i < Integer.parseInt(appProps.getProperty("num_of_products")); i++) {
 				System.out.println(ipg.generateQuery());
 			}
 		}
