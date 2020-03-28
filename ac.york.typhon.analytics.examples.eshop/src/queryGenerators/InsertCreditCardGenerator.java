9edef65c01d8eb923813dda87ea85599d21c0e43
Thanos Zolotas <zolotas4@gmail.com>
2020-03-28 08:00:57 +0000

    Users are now created including their credit card instances. Test consumer is also created.

--- ac.york.typhon.analytics.examples.eshop/src/queryGenerators/InsertCreditCardGenerator.java
+++ ac.york.typhon.analytics.examples.eshop/src/queryGenerators/InsertCreditCardGenerator.java
@@ -1,5 +1,7 @@
 package queryGenerators;
 
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import com.github.javafaker.Faker;
@@ -7,10 +9,11 @@ import com.github.javafaker.Faker;
 public class InsertCreditCardGenerator implements QueryGenerator {
 
 	@Override
-	public String generateQuery() {
+	public String generateQuery(Map<String, String> parameters) {
 		Faker faker = new Faker();
 		StringBuilder str = new StringBuilder();
 		str.append("insert CreditCard {");
+		str.append("id: \"" + parameters.get("CCUUID") + "\", ");
 		str.append("number: \"" + faker.finance().creditCard() + "\", ");
 		//FIXME: Check if date should be inside quotes or not
 		str.append("expiryDate: \"" + faker.date().future(730, TimeUnit.DAYS) + "\"");
