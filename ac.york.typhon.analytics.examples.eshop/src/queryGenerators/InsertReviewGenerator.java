22518a303d562521dd27eb7d6b977de2dad4305a
Thanos Zolotas <zolotas4@gmail.com>
2020-03-31 15:06:17 +0100

    Two more Agents. Order product is now either random or specific if product Id is passed in params.

--- ac.york.typhon.analytics.examples.eshop/src/queryGenerators/InsertReviewGenerator.java
+++ ac.york.typhon.analytics.examples.eshop/src/queryGenerators/InsertReviewGenerator.java
@@ -11,13 +11,18 @@ public class InsertReviewGenerator implements QueryGenerator {
 
 	@Override
 	public String generateQuery(Map<String, String> parameters) {
-		Random r = new Random();
-		String randomProductId =  RunSimulator.allProducts.get(r.nextInt(RunSimulator.allProducts.size()));
+		String productId = "";
+		if (!parameters.containsKey("productId")) {
+			Random r = new Random();
+			productId =  RunSimulator.allProducts.get(r.nextInt(RunSimulator.allProducts.size()));
+		} else {
+			productId =  parameters.get("productId");
+		}
 		Faker faker = new Faker();
 		StringBuilder str = new StringBuilder();
 		str.append("insert Review {");
 		str.append("content: \"" + faker.lorem().paragraph() + "\", ");
-		str.append("product: #" + randomProductId);
+		str.append("product: #" + productId);
 		str.append("}");
 		return str.toString();
 	}
