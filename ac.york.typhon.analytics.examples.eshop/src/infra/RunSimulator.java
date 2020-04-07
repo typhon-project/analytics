e22ccdd68bb815b2704a763dd060b935a80aad0f
Thanos Zolotas <zolotas4@gmail.com>
2020-04-07 09:59:30 +0100

    Refactored product creation.

--- ac.york.typhon.analytics.examples.eshop/src/infra/RunSimulator.java
+++ ac.york.typhon.analytics.examples.eshop/src/infra/RunSimulator.java
@@ -1,26 +1,18 @@
 package infra;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
-import java.util.UUID;
-
-import com.sun.jersey.api.client.ClientResponse;
 
 import agents.BuyerAgent;
 import agents.BuyerReviewerAgent;
 import agents.ReviewerNoBuyerAgent;
-import agents.SimpleAgent;
-import agents.SimpleAgent2;
+import entityCreator.ProductCreator;
 import queryGenerators.InsertAddressGenerator;
-import queryGenerators.InsertCreditCardGenerator;
-import queryGenerators.InsertProductGenerator;
 import queryGenerators.InsertUserGenerator;
 import utils.ExecuteQueries;
-import utils.ExecuteQueries.Utils;
 import utils.Utilities;
 
 public class RunSimulator {
@@ -53,9 +45,9 @@ public class RunSimulator {
 			InsertAddressGenerator iadg = new InsertAddressGenerator();
 
 			Map<String, String> params = new HashMap<String, String>();
+			int seed = Integer.parseInt(appProps.getProperty("seed"));
 			for (int i=0; i < Integer.parseInt(appProps.getProperty("num_of_users")); i++) {
-				int seed = Integer.parseInt(appProps.getProperty("seed"));
-				seed+=i;
+				seed++;
 				params.put("seed", ""  + seed);
 				// Create Address queries
 				// FIXME: After execution we need to keep the UUID to put it in the User query
@@ -67,48 +59,29 @@ public class RunSimulator {
 			System.out.println("User creation finished.");
 		}
 		// Store all users created
-		allUsers = Utilities.getAllUserUUIDs();
+//		allUsers = Utilities.getAllUserUUIDs();
 
 		System.out.println("Create products? " + appProps.getProperty("generate_products"));
 		
 		// Create Products
 		if(Boolean.parseBoolean(appProps.getProperty("generate_products"))) {
 			// Start by creating Categories
-			System.out.println("Started categories creation");
-			createCategories();
 			System.out.println("Started product creation");
-			InsertProductGenerator ipg = new InsertProductGenerator();
+			ProductCreator pc = new ProductCreator();
+			ArrayList<String> categoryIds = ProductCreator.createCategoryies();
+			// Create products
+			ArrayList<String> productIds = new ArrayList<String>();
+			int seed = Integer.parseInt(appProps.getProperty("seed"));
 			for (int i=0; i < Integer.parseInt(appProps.getProperty("num_of_products")); i++) {
-				utils.executeUpdate(ipg.generateQuery(null));
+				String productId = pc.create(seed, categoryIds);
+				productIds.add(productId);
+				seed++;
 			}
 			System.out.println("Product creation finished.");
 		}
 		// Store all products created
-		allProducts = Utilities.getAllProductUUIDs();
+//		allProducts = Utilities.getAllProductUUIDs();
 	}
 	
-	public static void createCategories() throws Exception {
-		ExecuteQueries eq = new ExecuteQueries();
-		ExecuteQueries.Utils utils = eq.new Utils();
-		
-		ArrayList<String> possibleCategories = new ArrayList<String>();
-		possibleCategories.add("Computers");
-		possibleCategories.add("Groceries");
-		possibleCategories.add("Health");
-		possibleCategories.add("Clothing");
-		possibleCategories.add("Kitchen");
-		possibleCategories.add("Bathroom");
-		possibleCategories.add("Furniture");
-		
-		for (int i=0; i<possibleCategories.size(); i++) {
-			String category = possibleCategories.get(i);
-			StringBuilder str = new StringBuilder();
-			str.append("insert Category {");
-			str.append("id: \"" + i + "\", ");
-			str.append("name: \"" + category + "\"");
-			str.append("}");
-			utils.executeUpdate(str.toString());
-		}
-	}
-
+	
 }
