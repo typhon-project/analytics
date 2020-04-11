fedda2484214735c3f968ce3ae0c67b4e1a3a387
Thanos Zolotas <zolotas4@gmail.com>
2020-04-11 14:58:17 +0100

    [EShop] Top users scenario

--- ac.york.typhon.analytics.examples.eshop/src/analytics/AnalyticsRunner.java
+++ ac.york.typhon.analytics.examples.eshop/src/analytics/AnalyticsRunner.java
@@ -9,7 +9,7 @@ import commons.EShopTopics;
 public class AnalyticsRunner {
 
 	public static void main(String[] args) throws Exception {
-		ChannelBuilder.build(new UndecisiveScenario(), AnalyticTopicType.POST, UUID.randomUUID().toString());
+		ChannelBuilder.build(new TopUsers(), AnalyticTopicType.POST, UUID.randomUUID().toString());
 	}
 
 }
