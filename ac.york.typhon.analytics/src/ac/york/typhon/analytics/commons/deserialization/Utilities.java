91e468a9b57f0102dc97f8a6ef3988d58ad0699f
Thanos Zolotas <zolotas4@gmail.com>
2020-04-20 08:27:34 +0100

    Inverted selects now select each field for the entity

--- ac.york.typhon.analytics/src/ac/york/typhon/analytics/commons/deserialization/Utilities.java
+++ ac.york.typhon.analytics/src/ac/york/typhon/analytics/commons/deserialization/Utilities.java
@@ -1,9 +1,13 @@
 package ac.york.typhon.analytics.commons.deserialization;
 
+import java.awt.List;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import ac.york.typhon.analytics.commons.datatypes.Entity;
 import engineering.swat.typhonql.ast.ASTConversionException;
+import engineering.swat.typhonql.ast.KeyVal;
 import engineering.swat.typhonql.ast.Request;
 import engineering.swat.typhonql.ast.Statement;
 import engineering.swat.typhonql.ast.Statement.Delete;
@@ -12,12 +16,12 @@ import engineering.swat.typhonql.ast.TyphonQLASTParser;
 import engineering.swat.typhonql.ast.Where;
 
 public class Utilities {
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		ExecuteQueries eq = new ExecuteQueries();
 		ExecuteQueries.Utils utils = eq.new Utils();
-		
+
 //		String query = "insert Category {name: \"cat 2\"}";
 //		String query = "from OrderedProduct op select op.@id, op.quantity";
 //		String query = "from Category c select c.@id, c.name";
@@ -46,9 +50,10 @@ public class Utilities {
 			// TODO: Implement this (only query invertor I believe and maybe store UUID)
 			DeleteDeserializer dd = new DeleteDeserializer();
 			dd.deserialize(query, resultSet);
-			
+
 		} else if (request.hasStm() && request.getStm().isUpdate()) {
-			// TODO: Implement this (re use insert code - maybee rename to upsert - but also an inverted select)
+			// TODO: Implement this (re use insert code - maybee rename to upsert - but also
+			// an inverted select)
 			UpdateDeserializer up = new UpdateDeserializer();
 		} else {
 			// It is a Select.
@@ -65,24 +70,31 @@ public class Utilities {
 //					"    ]\n" + 
 //					"  ]\n" + 
 //					"}";
-			if (request.hasStm() && request.getQry().getBindings().size()>1 ) {
+			if (request.hasStm() && request.getQry().getBindings().size() > 1) {
 				// TODO: This is a "join". We need to implement a "View" with slots
 			} else {
 				SelectDeserializer sd = new SelectDeserializer();
 				sd.deserialize(query, resultSet);
 			}
-			
+
 		}
 	}
-	
-	public String createInvertedSelect(Request request) {
+
+	public String createInvertedSelect(Request request) throws Exception {
 		Statement stm = request.getStm();
 		String dmlCommand = "";
 		if (request.getStm() instanceof Update || request.getStm() instanceof Delete) {
 			String eid = stm.getBinding().getEntity().getString();
 			String vid = stm.getBinding().getVar().getString();
-			// FIXME: QL does not support * at the moment. This needs to be set to the fields of the entity.
-			dmlCommand += "from " + eid + " " + vid + " select * ";
+			Class<?> objClass = Class.forName("ac.york.typhon.analytics.commons.datatypes." + eid);
+			Entity entity = (Entity) objClass.newInstance();
+			Field[] allFields = objClass.getDeclaredFields();
+			ArrayList<String> selectFieldsWithVids = new ArrayList<String>();
+			for (Field field : allFields) {
+				selectFieldsWithVids.add(vid + "." + field.getName());
+			}
+			String select = String.join(", ", selectFieldsWithVids);
+			dmlCommand += "from " + eid + " " + vid + " select " + select + " ";
 
 			if (stm.hasWhere()) {
 				Where where = stm.getWhere();
